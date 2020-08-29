0ee5ff01e1e7e3bb6708e7cfe61c09b5ee3cca1d
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-29 06:16:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = batteryBatches.reduce(function(totalCount, batches){
   return batches + totalCount
   }
-  //for each time we  iterate through batteryBatches, add the element  to the total 
+  //for each time we  iterate through batteryBatches, add the current element  to the totalCount
 )
