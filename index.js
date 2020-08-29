8492885227de32674d2be152f82b5fd87be444f1
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-29 06:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
-
-
-function reduceBatteries(array, reducer, initialValue) {
-  let accumulator = initialValue;
-  array.forEach(battery => {accumulator = reducer(accumulator, battery);
-  });
-  return accumulator; 
-}
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+  return batches + total
+  }
+)
